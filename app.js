7080962caf6fe4035ad5cc41ae110fcaf72627ef
matt-winzer <matt.winzer@gmail.com>
2018-02-13 13:29:27 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const ability = require('./pokemon.json')
+
+// Write a function that takes in an ability object, and returns (or logs):
+  // The ability name
+
+
+
+// Write a function that takes in an ability object, and returns (or logs):
+  // An object with the ability name and generation
+
+
+
+// Write a function that takes in an ability object, and returns (or logs):
+  // An array of all the pokemon that can use this ability
+
+
+
+// Write a function that takes in an ability object, and returns (or logs):
+  // An object with the ability id, name, and an array of all the pokemon that can use the ability
+
---
+++ object-navigation.md
@@ -0,0 +1,10 @@
+# Object & Array Navigation
+
+## Objectives
+
+- Explain some benefits of nested objects
+- Access data from the first layer of an object
+- Access data from an array nested in an object
+- Combine functions to sanitize data
+
+## Notes
